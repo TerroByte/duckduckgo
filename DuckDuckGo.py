61a7d252b72febb1118833cfe16c69fc488baabc
Evan Spiering <43457599+TerroByte@users.noreply.github.com>
2021-10-22 13:09:43 -0400

    Update duckduckgo.py

--- DuckDuckGo.py
+++ DuckDuckGo.py
@@ -1,8 +1,14 @@
-import pytest
-url_ddg = "https://api.duckduckgo.com"
+import requests
+url = "https://duckduckgo.com/?q=presidents+of+the+united+states&format=json"
+presidents = [" Washington", " Adams", " Jefferson", " Madison", " Monroe", " Adams", " Jackson", " Buren", " Harrison", " Tyler", " Polk", " Taylor", " Fillmore", " Pierce",
+              " Buchanan", " Lincoln", " Johnson", " Grant", " Hayes", " Garfield", " Arthur", " Cleveland", " Harrison", " Cleveland", " McKinley", " Roosevelt", " Taft",
+              " Wilson", " Harding", " Coolidge", " Hoover", " Roosevelt", " Truman", " Eisenhower", " Kennedy", " Johnson", " Nixon", " Ford", " Carter", " Reagan", " Bush",
+              " Clinton", " Obama", " Trump", " Biden"]
 
 def test_ddg0():
-    resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
+    resp = requests.get(url + "/?q=DuckDuckGo&format=json")
     rsp_data = resp.json()
-    assert "DuckDuckGo" in rsp_data["Heading"]
-    
+    for x in presidents:
+        assert x in str(rsp_data)
+
+
